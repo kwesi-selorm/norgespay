fdf8679dd1acb1101af01851a27aedd2f55329bd
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-03-31 19:24:59 +0200

    Initial commit.

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
