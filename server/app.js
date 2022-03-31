6154e2eef39d0ddca88c8d1c799d0e5751d4b0c5
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-03-31 19:40:06 +0200

    Initialise React client side and update server code.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
