3588afa927d6ee68674ef747aea51f8cb4de53e5
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-04-14 13:47:27 +0200

    Remove model methods

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,23 +1,10 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import passportLocalMongoose from "passport-local-mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true }, //email input
   password: { type: String, required: true },
 });
-
-//METHODS
-//Generating a hash with salt
-userSchema.methods.generateHash = (password) => {
-  return bcrypt.hashSync(password, genereateSaltSync(12), null);
-};
-
-//Checking if a password is valid
-userSchema.methods.validPassword = (password) => {
-  return bcrypt.compareSync(password, this.local.password);
-};
 
 //The third argument implies saving documents based on the User schema in the users collection
 const User = mongoose.model("User", userSchema, "users");
