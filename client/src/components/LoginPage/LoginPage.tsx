f8a72378372a96141943fa4e92507ca49e40973e
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-04-18 12:36:28 +0200

    Remove action and method attributes from form and use onSubmit with type=submit button. Axios to handle requests.

--- client/src/components/LoginPage/LoginPage.tsx
+++ client/src/components/LoginPage/LoginPage.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "../../globals.css";
 
 const LoginPage = () => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").textContent;
+    const password = document.getElementById("pwd").textContent;
+
+    axios.post(
+      "/user/login",
+      { username, password },
+      { headers: { "Content-Type": "application/json" } }
+    );
+  };
+
   return (
     <div className="login-signup-div">
-      <form action="/user/login" method="post">
+      <form onSubmit={handleSubmit}>
         <h1 className="login-signup-form-title">Log In</h1>
         <label htmlFor="email" className="form-label">
           Email
@@ -22,7 +36,9 @@ const LoginPage = () => {
           Password
         </label>
         <input className="input" type="password" name="pwd" id="pwd" required />
-        <button className="login-signup-button">Submit</button>
+        <button className="login-signup-button" type="submit">
+          Submit
+        </button>
       </form>
       <p>Forgot password?</p>
       <Link to="/signup" style={{ textDecoration: "none", fontWeight: "bold" }}>
