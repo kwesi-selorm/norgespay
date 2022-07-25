79284bc6075c1605e94b585de89df2684e59fc30
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-07-25 16:42:43 +0200

    Update function

--- client/src/utils/setErrorMessage.ts
+++ client/src/utils/setErrorMessage.ts
@@ -25,7 +25,6 @@ export function setErrorMessage({ error, setNotification }: ErrorParams) {
     });
     setTimeout(() => {
       setNotification({ className: null, message: "" });
-      // navigate("/signup");
     }, 5000);
   }
 }
