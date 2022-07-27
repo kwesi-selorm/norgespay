ed2af9b6bfc259abdab98129e818c7d924f72006
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-07-28 00:55:47 +0200

    Implement QueryClientProvider to pass on client to App children

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+      <ReactQueryDevtools position="bottom-right" />
+    </QueryClientProvider>
   </React.StrictMode>
 );
