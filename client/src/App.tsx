1aac616e1909d8845199c2d17428026784aa8e03
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-07-28 12:36:55 +0200

    Add TODOs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,13 @@ import { User } from "./types";
 import NotFound from "./views/NotFound/NotFound";
 import Layout from "./components/Layout";
 
+/*TODO: Add transition to salary cards when they are hovered upon (increase scaling); 
+On clicking a salary card, a new page should open with further details on the card (sector, years of experience, median salary value, maximum and minimum salary, chart of salary progression?, conversion tool);
+Not critical: Add box shadow to salary cards, use box shadows of login and signup forms as refs;
+Implement react query mutations; Add new view component for displaying a single salary card, route possibly /salary/:id
+Add user field to salary schema in mongoose
+*/
+
 function App() {
   const [user, setUser] = useState(null);
   const [loggedUser, setLoggedUser] = useState<User>(null);
