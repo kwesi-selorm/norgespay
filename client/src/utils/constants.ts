94f77d1a764de1d2b7bba3a2d140d408d88cab25
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-07-31 21:49:08 +0200

    Create global constants

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,10 @@
+import { NewSalaryInputs } from "../types";
+
+export const initialNotification = { message: "", className: "" };
+
+export const initialInputs: NewSalaryInputs = {
+  jobTitle: "",
+  company: "",
+  salary: 0,
+  city: "",
+};
