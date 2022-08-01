b3d639fb3292bea63fa144c3600c7437603a2925
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-01 09:49:42 +0200

    Move to utils directory

---
+++ client/src/utils/types.ts
@@ -0,0 +1,40 @@
+export type Salary = {
+  jobTitle: string;
+  salary: number[];
+  company: string;
+  city: string;
+  id?: string;
+  dateAdded: string;
+};
+
+export interface SalaryCardProps {
+  jobTitle: string;
+  company: string;
+  salary: number;
+  city: string;
+  id?: string;
+  dateAdded?: string;
+}
+
+export interface User {
+  username: string;
+  token: string;
+  id: string;
+}
+
+export interface LoginProps {
+  user: User;
+  setUser: (user: User) => void;
+}
+
+export type NewSalaryInputs = {
+  jobTitle: string;
+  company: string;
+  salary: number;
+  city: string;
+};
+
+export type NewNotification = {
+  message: string;
+  className: string;
+};
