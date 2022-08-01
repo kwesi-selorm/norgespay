6b7dde76c2c4b68fbd32bbe1613ea95e088d2094
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-01 12:33:24 +0200

    Add sectors array constant

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -8,3 +8,29 @@ export const initialInputs: NewSalaryInputs = {
   salary: 0,
   city: "",
 };
+
+/*
+TODO: Write a chooseSectorValue function when a new salary is created and store as the 'sector' field value
+*/
+export const sectors: Object[] = [
+  { Agriculture: "agriculture" },
+  { Business: "business" },
+  { "Communication Services": "communications" },
+  { Construction: "construction" },
+  { Consulting: "consulting" },
+  { "Consumer Staples and Discretionary": "consumer" },
+  { "Education and Research": "education" },
+  { "Energy (O&G, Renewables,etc.)": "energy" },
+  { Entertainment: "entertainment" },
+  { "Financials (Banking, Insurance, etc.)": "financial" },
+  { "General Engineering": "engineering" },
+  { Healthcare: "healthcare" },
+  { Industrials: "industry" },
+  { "Information Technology": "tech" },
+  { "Legal and Consulting Services": "legal-consulting" },
+  { Materials: "materials" },
+  { "Real Estate": "real-estate" },
+  { Utilities: "utilities" },
+  { "Waste Management": "waste" },
+  { Others: "others" },
+];
