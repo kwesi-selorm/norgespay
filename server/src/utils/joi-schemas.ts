a50e89b207f137570017a9bd493dd1e66ea4f7b7
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-02 01:31:31 +0200

    Add RegExp for password pattern

--- server/src/utils/joi-schemas.ts
+++ server/src/utils/joi-schemas.ts
@@ -30,6 +30,10 @@ export const loginSchema = Joi.object({
 export const signUpSchema = Joi.object({
   email: Joi.string().email().required(),
   username: Joi.string().min(3).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string()
+    .pattern(
+      new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
+    )
+    .required(),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
 });
