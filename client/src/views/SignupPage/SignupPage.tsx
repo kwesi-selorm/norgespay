32f20ac040a681c0f7f8bd56bb31ea7e6ca8795e
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-02 02:04:37 +0200

    Add confirmPassword to req body, implement api in component, insert Notification component to improve user experience

--- client/src/views/SignupPage/SignupPage.tsx
+++ client/src/views/SignupPage/SignupPage.tsx
@@ -1,28 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { submitSignupDetails } from "../../api/signup";
+import { useRecoilState } from "recoil";
+import Notification from "../../components/Notification";
+import { useNotification } from "../../hooks/useNotification";
+import { notificationState } from "../../recoil/atoms";
 import "../../styles/LoginSignup.css";
 
-//TODO: Allow only legitimate email addresses and accounts
 const SignupPage = () => {
+  const [signupDetails, setSignupDetails] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { display, createSuccess, createError } = useNotification();
+  const [notification, setNotification] = useRecoilState(notificationState);
   const navigate = useNavigate();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const email = e.target.email.value,
-      username = e.target.username.value,
-      password = e.target.password.value;
 
     try {
-      const { data } = await submitSignupDetails({ email, username, password });
+      const data = { ...signupDetails };
+      const config = { headers: { "Content-Type": "application/json" } };
+      await axios.post("/api/signup", data, config);
+      setNotification(
+        createSuccess("New user added successfully, redirecting...")
+      );
       navigate("/login");
-      console.log(data);
     } catch (error: any) {
-      console.error("Something went wrong: " + error.message);
+      if (error instanceof Error) {
+        setNotification(createError(error));
+      }
+      if (error.response) {
+        //Catch axios error
+        setNotification(createError(error.response.data));
+      }
     }
   };
 
   return (
     <div className="login-signup-div">
+      <Notification
+        display={display}
+        message={notification.message}
+        className={notification.className}
+      />
       <form onSubmit={handleSubmit}>
         <h1 className="login-signup-form-title">Sign Up</h1>
         <label htmlFor="email" className="form-label">
@@ -33,30 +57,60 @@ const SignupPage = () => {
           type="email"
           name="email"
           id="email"
+          value={signupDetails.email}
           required
           autoFocus
+          onChange={({ target }) => {
+            setSignupDetails({ ...signupDetails, email: target.value });
+          }}
         />
         <label htmlFor="username" className="form-label">
           Username
+          <input
+            className="input"
+            type="username"
+            name="username"
+            id="username"
+            value={signupDetails.username}
+            required
+            onChange={({ target }) => {
+              setSignupDetails({ ...signupDetails, username: target.value });
+            }}
+          />
         </label>
-        <input
-          className="input"
-          type="username"
-          name="username"
-          id="username"
-          required
-          autoFocus
-        />
         <label htmlFor="password" className="form-label">
           Password
+          <input
+            className="input"
+            type="password"
+            name="password"
+            id="password"
+            value={signupDetails.password}
+            title="Password must have at least one each of a lowercase letter, an upper case letter, a number, and a special character.It must be at least 8 characters long"
+            required
+            onChange={({ target }) => {
+              setSignupDetails({ ...signupDetails, password: target.value });
+            }}
+          />
         </label>
-        <input
-          className="input"
-          type="password"
-          name="password"
-          id="password"
-          required
-        />
+        <label htmlFor="confirm-password" className="form-label">
+          Confirm Password
+          <input
+            className="input"
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            value={signupDetails.confirmPassword}
+            title="Passwords must match"
+            required
+            onChange={({ target }) => {
+              setSignupDetails({
+                ...signupDetails,
+                confirmPassword: target.value,
+              });
+            }}
+          />
+        </label>
         <p>
           Already a user?{" "}
           <Link
