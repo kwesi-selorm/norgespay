17e67411ec08121f4229126d5fb6837c7e57e13f
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-03 00:16:16 +0200

    Add button and implement redirect function to a selected salary card's unique page

--- client/src/components/SalaryCard/index.tsx
+++ client/src/components/SalaryCard/index.tsx
@@ -5,12 +5,34 @@ import LeftSection from "./LeftSection";
 import RightSectionTop from "./RightSectionTop";
 import RightSectionMid from "./RightSectionMid";
 import RightSectionBottom from "./RightSectionBottom";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { salariesState } from "../../recoil/atoms";
+import { IoChevronForwardCircleSharp } from "react-icons/io5";
 
 //TODO: Add conversion elements to USD, GBP, etc. Consider using percentiles instead of averages.
 
-const SalaryCard = ({ jobTitle, company, city, ...props }: SalaryCardProps) => {
-  const [display, setDisplay] = useState("none"),
-    [, setUserInput] = useState<number>(0);
+const SalaryCard = ({
+  jobTitle,
+  company,
+  city,
+  id,
+  ...props
+}: SalaryCardProps) => {
+  const [display, setDisplay] = useState("none");
+  const [, setUserInput] = useState<number>(0);
+  const salaries = useRecoilValue(salariesState);
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    const salary = salaries.find(
+      (salary) =>
+        salary.jobTitle === jobTitle &&
+        salary.company === company &&
+        salary.city === city
+    );
+    navigate(`/all-salaries/${salary.id}`);
+  };
 
   return (
     <div className="salary-card">
@@ -32,6 +54,11 @@ const SalaryCard = ({ jobTitle, company, city, ...props }: SalaryCardProps) => {
         />
         <RightSectionBottom dateAdded={props.dateAdded} />
       </div>
+      <IoChevronForwardCircleSharp
+        className="forward-button"
+        style={{ color: "green" }}
+        onClick={handleRedirect}
+      />
     </div>
   );
 };
